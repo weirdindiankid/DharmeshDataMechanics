aa099d33303161761d0617b5b35b4e45ba9dfbcf
Dharmesh Tarapore <dharmesh@bu.edu>
2018-04-29 22:59:38 -0400

    V1.0.6

--- DharmeshDataMechanics/CS591/__init__.py
+++ DharmeshDataMechanics/CS591/__init__.py
@@ -1,9 +1,12 @@
 
 BASE_AUTH = "bemullen_crussack_dharmesh_vinwah"
 CONTRIBUTOR = "bemullen_crussack_dharmesh_vinwah"
+BASE_NAME = "bemullen_crussack_dharmesh_vinwah"
 
-def setBaseAuth(baseAuth):
+def setBaseName(baseAuth):
     global BASE_AUTH
+    global BASE_NAME
     global CONTRIBUTOR
     BASE_AUTH = baseAuth
     CONTRIBUTOR = baseAuth
+    BASE_NAME = baseAuth
