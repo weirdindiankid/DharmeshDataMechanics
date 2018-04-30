e7789cf4efa01ca9527bb19f26bf43f611905890
Dharmesh Tarapore <dharmesh@bu.edu>
2018-04-29 22:19:02 -0400

    Initial commit

---
+++ DharmeshDataMechanics/CS591/__init__.py
@@ -0,0 +1,9 @@
+
+BASE_AUTH = "bemullen_crussack_dharmesh_vinwah"
+CONTRIBUTOR = "bemullen_crussack_dharmesh_vinwah"
+
+def setBaseAuth(baseAuth):
+    global BASE_AUTH
+    global CONTRIBUTOR
+    BASE_AUTH = baseAuth
+    CONTRIBUTOR = baseAuth
